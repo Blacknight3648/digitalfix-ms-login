41dae4c5ce646a5263a363df4a0b5f3fea9832d2
Cristopher Osses <cr.ossesg@duocuc.cl>
2026-09-08 00:50:24 -0300

    chore: inicializar microestructura del ms-login y dependencias

---
+++ src/main/java/df/digitalfix_ms_login/DigitalfixMsLoginApplication.java
@@ -0,0 +1,13 @@
+package df.digitalfix_ms_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalfixMsLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalfixMsLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/df/digitalfix_ms_login/DigitalfixMsLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package df.digitalfix_ms_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalfixMsLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
